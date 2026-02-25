f29e2a9362266db6fa2264135f41c33b905b138d
joel06rob-OOPDesktop <User@DESKTOP-54QM2KU>
2026-02-25 20:15:45 +0000

    Chat screen

---
+++ ContactChat.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOP_NW_Airbnb
+{
+    /// <summary>
+    /// Interaction logic for ContactChat.xaml
+    /// </summary>
+    public partial class ContactChat : Window
+    {
+        public ContactChat()
+        {
+            InitializeComponent();
+        }
+    }
+}
